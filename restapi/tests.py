93bacdd994719555cdf831d77e575ec828513156
Mepecapt <rahinzaman26@gmail.com>
2023-02-12 01:43:37 +0600

    Added the first test & github actions.

---
+++ restapi/tests.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+# Create your tests here.
+
+def two_integers_sum(a, b):
+    return a + b
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integers_sum(5, 6), 11)
